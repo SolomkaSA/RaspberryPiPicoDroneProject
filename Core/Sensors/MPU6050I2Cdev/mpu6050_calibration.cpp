824a0b8c4dc0c617707effd32402250d50df4a3e
Sergii Solomka <sergii.solomka+github@gmail.com>
2024-10-05 18:23:03 +0300

    Migrate project to Pico Tool Template

--- Core/Sensors/MPU6050I2Cdev/mpu6050_calibration.cpp
+++ Core/Sensors/MPU6050I2Cdev/mpu6050_calibration.cpp
@@ -241,13 +241,13 @@ int Calibrate()
   // setup blink led
   gpio_init(PICO_DEFAULT_LED_PIN);
   gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
-  while (!stdio_usb_connected())
-  { // blink the pico's led until usb connection is established
-    gpio_put(PICO_DEFAULT_LED_PIN, 1);
-    sleep_ms(250);
-    gpio_put(PICO_DEFAULT_LED_PIN, 0);
-    sleep_ms(250);
-  }
+  // while (!stdio_usb_connected())
+  // { // blink the pico's led until usb connection is established
+  //   gpio_put(PICO_DEFAULT_LED_PIN, 1);
+  //   sleep_ms(250);
+  //   gpio_put(PICO_DEFAULT_LED_PIN, 0);
+  //   sleep_ms(250);
+  // }
 
   Initialize();
   for (int i = iAx; i <= iGz; i++)
